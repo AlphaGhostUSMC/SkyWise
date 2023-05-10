ea3f51287d3efd42de1a0e678f8751089bfbfcd5
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-05-09 23:58:49 -0700

    API Fetch completed for main weather widget

---
+++ assets/js/fetch-weather.js
@@ -0,0 +1,54 @@
+// Set API endpoint
+const endpoint = "https://api.openweathermap.org/data/2.5/weather";
+
+// Set API key
+const apiKey = "1d4298744e7a95525f475935e6ec25db";
+
+// Get the search input element
+const searchInput = document.querySelector("#location-search-input");
+
+// Get the weather data based on the search input
+async function getWeatherData() {
+  try {
+    // Construct the API URL with the search input and API key
+    const url = `${endpoint}?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // Display weather data in the UI
+    displayWeather(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Update the weather data when the search form is submitted
+const searchForm = document.querySelector(".get-weather");
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherData();
+});
+
+// Update the weather data when the page loads
+window.addEventListener("load", () => {
+  getWeatherData();
+});
+
+function displayWeather(data) {
+  const { name, main, weather } = data;
+  const cityMain = document.querySelector(".city-name-main");
+  const tempMain = document.querySelector(".temp-main");
+  const descriptionMain = document.querySelector(".temp-description");
+  const weatherIcon = {
+    "overcast clouds": "assets/img/weather-icons/cloudy.png",
+    "haze": "assets/img/weather-icons/haze.png",
+    "clear sky": "assets/img/weather-icons/sunny.png",
+    "few clouds": "assets/img/weather-icons/partly-cloudy.png"
+  };
+  const currentWeatherIcon = document.querySelector(".weather-icon-main");
+  const weatherDescription = weather[0].description;
+
+  cityMain.textContent = name + ", " + data.sys.country;
+  tempMain.textContent = main.temp + " ℃";
+  descriptionMain.textContent = weather[0].description;
+  currentWeatherIcon.src = weatherIcon[weatherDescription];
+}
