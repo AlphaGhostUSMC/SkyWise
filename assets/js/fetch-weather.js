b9316e35eeaee92a0c4e749412b847462282750c
QWxwaGEgU2lsdmVyQmFjaw== <90997461+AlphaGhostUSMC@users.noreply.github.com>
2023-05-11 20:19:27 +0530

    fetching forecast data in progress

--- assets/js/fetch-weather.js
+++ assets/js/fetch-weather.js
@@ -147,10 +147,52 @@ function displayForecast(data) {
   const forecastDay1Hour2 = document.querySelector(".forecast-day1-hour2");
   const forecastDay1Hour3 = document.querySelector(".forecast-day1-hour3");
   const forecastDay1Hour4 = document.querySelector(".forecast-day1-hour4");
-
+  const forecastDay2Hour1 = document.querySelector(".forecast-day2-hour1");
+  const forecastDay2Hour2 = document.querySelector(".forecast-day2-hour2");
+  const forecastDay2Hour3 = document.querySelector(".forecast-day2-hour3");
+  const forecastDay2Hour4 = document.querySelector(".forecast-day2-hour4");
+  const forecastDay3Hour1 = document.querySelector(".forecast-day3-hour1");
+  const forecastDay3Hour2 = document.querySelector(".forecast-day3-hour2");
+  const forecastDay3Hour3 = document.querySelector(".forecast-day3-hour3");
+  const forecastDay3Hour4 = document.querySelector(".forecast-day3-hour4");
 
   forecastDay1Hour1.textContent = list[4].dt_txt.slice(11, 16);
   forecastDay1Hour2.textContent = list[5].dt_txt.slice(11, 16);
   forecastDay1Hour3.textContent = list[6].dt_txt.slice(11, 16);
   forecastDay1Hour4.textContent = list[7].dt_txt.slice(11, 16);
+  forecastDay2Hour1.textContent = list[12].dt_txt.slice(11, 16);
+  forecastDay2Hour2.textContent = list[13].dt_txt.slice(11, 16);
+  forecastDay2Hour3.textContent = list[14].dt_txt.slice(11, 16);
+  forecastDay2Hour4.textContent = list[15].dt_txt.slice(11, 16);
+  forecastDay3Hour1.textContent = list[20].dt_txt.slice(11, 16);
+  forecastDay3Hour2.textContent = list[21].dt_txt.slice(11, 16);
+  forecastDay3Hour3.textContent = list[22].dt_txt.slice(11, 16);
+  forecastDay3Hour4.textContent = list[23].dt_txt.slice(11, 16);
+
+
+  const forecastDay1Hour1Temp = document.querySelector(".forecast-day1-hour1-temp");
+  const forecastDay1Hour2Temp = document.querySelector(".forecast-day1-hour2-temp");
+  const forecastDay1Hour3Temp = document.querySelector(".forecast-day1-hour3-temp");
+  const forecastDay1Hour4Temp = document.querySelector(".forecast-day1-hour4-temp");
+  const forecastDay2Hour1Temp = document.querySelector(".forecast-day2-hour1-temp");
+  const forecastDay2Hour2Temp = document.querySelector(".forecast-day2-hour2-temp");
+  const forecastDay2Hour3Temp = document.querySelector(".forecast-day2-hour3-temp");
+  const forecastDay2Hour4Temp = document.querySelector(".forecast-day2-hour4-temp");
+  const forecastDay3Hour1Temp = document.querySelector(".forecast-day3-hour1-temp");
+  const forecastDay3Hour2Temp = document.querySelector(".forecast-day3-hour2-temp");
+  const forecastDay3Hour3Temp = document.querySelector(".forecast-day3-hour3-temp");
+  const forecastDay3Hour4Temp = document.querySelector(".forecast-day3-hour4-temp");
+
+  forecastDay1Hour1Temp.textContent = list[4].main.temp + " ℃";
+  forecastDay1Hour2Temp.textContent = list[5].main.temp + " ℃";
+  forecastDay1Hour3Temp.textContent = list[6].main.temp + " ℃";
+  forecastDay1Hour4Temp.textContent = list[7].main.temp + " ℃";
+  forecastDay2Hour1Temp.textContent = list[12].main.temp + " ℃";
+  forecastDay2Hour2Temp.textContent = list[13].main.temp + " ℃";
+  forecastDay2Hour3Temp.textContent = list[14].main.temp + " ℃";
+  forecastDay2Hour4Temp.textContent = list[15].main.temp + " ℃";
+  forecastDay3Hour1Temp.textContent = list[20].main.temp + " ℃";
+  forecastDay3Hour2Temp.textContent = list[21].main.temp + " ℃";
+  forecastDay3Hour3Temp.textContent = list[22].main.temp + " ℃";
+  forecastDay3Hour4Temp.textContent = list[23].main.temp + " ℃";
 }
