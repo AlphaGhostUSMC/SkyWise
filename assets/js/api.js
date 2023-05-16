7d4353ea51cbd7919e69629613df6e0ff9f53159
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-05-15 17:44:42 -0700

    Cleanup, Main Widget data fetch, user mgmt in prog

---
+++ assets/js/api.js
@@ -0,0 +1,5 @@
+const endpointWeather = "https://api.openweathermap.org/data/2.5/weather";
+const endpointForecast = "https://api.openweathermap.org/data/2.5/forecast";
+const endpointSunsetSunrise = "https://api.sunrisesunset.io/json?";
+const endpointCoordinates = "https://api.openweathermap.org/geo/1.0/direct"
+const apiKey = "1d4298744e7a95525f475935e6ec25db";
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
-const endpointWeather = "https://api.openweathermap.org/data/2.5/weather";
-const endpointForecast = "https://api.openweathermap.org/data/2.5/forecast";
-const endpointSunsetSunrise = "https://api.sunrisesunset.io/json?";
-const endpointCoordinates = "https://api.openweathermap.org/geo/1.0/direct"
-const apiKey = "1d4298744e7a95525f475935e6ec25db";
-
 const searchInput = document.querySelector("#location-search-input");
 
 const searchForm = document.querySelector(".get-weather");
@@ -90,17 +84,6 @@ function displayWeather(data) {
   const cityMain = document.querySelector(".city-name-main");
   const tempMain = document.querySelector(".temp-main");
   const descriptionMain = document.querySelector(".temp-description");
-  const weatherIcon = {
-    "overcast clouds": "assets/img/weather-icons/overcast-clouds.png",
-    "few clouds": "assets/img/weather-icons/partly-cloud.png",
-    "broken clouds": "assets/img/weather-icons/broken-clouds.png",
-    "scattered clouds": "assets/img/weather-icons/scattered-clouds.png",
-    haze: "assets/img/weather-icons/haze.png",
-    "clear sky": "assets/img/weather-icons/sunny.png",
-    "light rain": "assets/img/weather-icons/light-rain.png",
-    "mist": "assets/img/weather-icons/mist.png",
-    "moderate rain": "assets/img/weather-icons/moderate-rain.png",
-  };
   const currentWeatherIcon = document.querySelector(".weather-icon-main");
   const weatherDescription = weather[0].description;
 
@@ -152,25 +135,6 @@ function displayWeather(data) {
   const deg = data.wind.deg;
   const direction = wind_direction(deg);
   console.log("Wind direction: " + direction);
-
-  const directionIcon = {
-    N: "assets/img/directions/N.png",
-    NNE: "assets/img/directions/NNE.png",
-    NE: "assets/img/directions/NE.png",
-    ENE: "assets/img/directions/ENE.png",
-    E: "assets/img/directions/E.png",
-    ESE: "assets/img/directions/ESE.png",
-    SE: "assets/img/directions/SE.png",
-    SSE: "assets/img/directions/SSE.png",
-    S: "assets/img/directions/S.png",
-    SSW: "assets/img/directions/SSW.png",
-    SW: "assets/img/directions/SW.png",
-    WSW: "assets/img/directions/WSW.png",
-    W: "assets/img/directions/W.png",
-    WNW: "assets/img/directions/WNW.png",
-    NW: "assets/img/directions/NW.png",
-    NNW: "assets/img/directions/NNW.png",
-  };
 
   const windDirectionIcon = document.querySelector(".wind-direction-icon");
   windDirectionIcon.src = directionIcon[direction];
