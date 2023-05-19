c3aeb84bf4ebb1c3003e54b88279dd8cc7b877e6
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-05-19 01:05:31 -0700

    Included Password Match Indicator

--- assets/js/valAuth.js
+++ assets/js/valAuth.js
@@ -3,11 +3,10 @@ const emailInput = document.querySelector('.email-input');
 const passwordInput = document.querySelector('.password-input');
 const cnfPasswordInput = document.querySelector('.cnfpassword-input');
 const registerBtn = document.querySelector('.register');
-// const.eyeIcon = document.querySelector('#eyeIcon');
 
 function togglePasswordVisibility(inputId, iconId) {
-  var passwordInput = document.getElementById(inputId);
-  var eyeIcon = document.getElementById(iconId);
+  const passwordInput = document.getElementById(inputId);
+  const eyeIcon = document.getElementById(iconId);
 
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
@@ -29,4 +28,24 @@ function showError(input) {
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'input-container success';
+}
+
+function passwordMatchIndicator() {
+  const passwordInput = document.getElementById("password");
+  const cnfPasswordInput = document.getElementById("cnfpassword");
+  const passwordIcon = document.getElementById("key1");
+  const cnfPasswordIcon = document.getElementById("key2");
+
+  if (passwordInput.value === cnfPasswordInput.value) {
+    passwordIcon.style.color = "var(--success-color)";
+    cnfPasswordIcon.style.color = "var(--success-color)";
+  } else {
+    passwordIcon.style.color = "var(--error-color)";
+    cnfPasswordIcon.style.color = "var(--error-color)";
+  }
+
+  if (passwordInput.value === "" && cnfPasswordInput.value === "") {
+    passwordIcon.style.removeProperty("color");
+    cnfPasswordIcon.style.removeProperty("color");
+  }
 }
