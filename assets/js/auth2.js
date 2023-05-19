04a961f72f49e4f84ae7adb7cb6ab9a859a17878
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-05-19 05:27:25 -0700

    Testing FireBase Auth

---
+++ assets/js/auth2.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAjSXSBDKv6dHzLEFbA_0vGgOVrNlx9MYc",
+    authDomain: "skywise-alpha.firebaseapp.com",
+    projectId: "skywise-alpha",
+    storageBucket: "skywise-alpha.appspot.com",
+    messagingSenderId: "83174467274",
+    appId: "1:83174467274:web:07a94215b801e87b51b633"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
