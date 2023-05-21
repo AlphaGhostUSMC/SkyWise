c769d0522641b83a7372a2953def46ebf12ff63f
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-05-21 00:06:57 -0700

    Nil

--- assets/js/auth2.js
+++ assets/js/auth2.js
@@ -1,17 +1,41 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const { MongoClient } = require('mongodb');
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAjSXSBDKv6dHzLEFbA_0vGgOVrNlx9MYc",
-    authDomain: "skywise-alpha.firebaseapp.com",
-    projectId: "skywise-alpha",
-    storageBucket: "skywise-alpha.appspot.com",
-    messagingSenderId: "83174467274",
-    appId: "1:83174467274:web:07a94215b801e87b51b633"
-  };
+const uri = 'mongodb+srv://AlphaDBAdmin2:Zoo05rF9PMR3UeGG@alphadbv1.9q93m.mongodb.net/SkyWise?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+client.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const collection = client.db('mytestdb').collection('users');
+
+  const username = document.querySelector('.reg-username-input').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+  const cnfpassword = document.querySelector('#cnfpassword').value.trim();
+
+  // Input validation
+  if (!username || !email || !password || !cnfpassword) {
+    console.log('All fields are required');
+    return;
+  }
+
+  if (password !== cnfpassword) {
+    console.log('Password and confirm password do not match');
+    return;
+  }
+
+  const user = { username, email, password };
+
+  collection.insertOne(user, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('User stored to MongoDB Atlas database');
+    client.close();
+  });
+
+});
